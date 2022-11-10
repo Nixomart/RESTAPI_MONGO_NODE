2d20ea5fe9a528c8f571fb9d46bb35dde56c8c81
Nixomart <nixoma1@gmail.com>
2022-11-10 20:50:43 -0300

    now node js removes the image when modifying the post

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -11,15 +11,15 @@ export const addPost = async (req, res) => {
   try {
 
     const { title, description } = req.body;
-    let image;
+    let image=null;
     //validacion si imagen existe
     if(req.files?.image){
       const result = await uploadImage(req.files.image.tempFilePath)
+      await fs.remove(req.files.image.tempFilePath)
       image = {
         url: result.secure_url,
         public_id: result.public_id
       }
-      await fs.remove(req.files.image.tempFilePath)
     }
 
     const newPost = new Post({ title, description, image: image });
@@ -32,9 +32,35 @@ export const addPost = async (req, res) => {
 
 export const updatePost = async (req, res) => {
   try {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    console.log(post)
-    return res.json(post);
+    const {id} = req.params
+
+    //agrega
+    const post = await Post.findById(req.params.id)
+    await deleteImage(post.image.public_id)
+
+
+    if(req.files?.image){
+
+      const result = await uploadImage(req.files.image.tempFilePath)
+      await fs.remove(req.files.image.tempFilePath)
+      req.body.image = {
+        url: result.secure_url,
+        public_id: result.public_id
+      }
+
+    }
+    
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      {
+        new: true,
+      }
+      )
+      
+      console.log(Post.findById(id))
+
+    return res.json(updatedPost);
   } catch (error) {}
   res.send("posts update");
 };
