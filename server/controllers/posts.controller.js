0f325caa53fca61b2cc2e1b09a45ba59e2bad062
Nixomart <nixoma1@gmail.com>
2022-11-12 13:40:34 -0300

    updating post fixed, now it updating without need for an image

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -1,6 +1,6 @@
 import Post from "../models/Post.js";
-import {deleteImage, uploadImage} from '../libs/cloudinary.js'
-import fs from 'fs-extra'
+import { deleteImage, uploadImage } from "../libs/cloudinary.js";
+import fs from "fs-extra";
 
 export const getPosts = async (req, res) => {
   const posts = await Post.find();
@@ -9,17 +9,16 @@ export const getPosts = async (req, res) => {
 
 export const addPost = async (req, res) => {
   try {
-
     const { title, description } = req.body;
-    let image=null;
+    let image = null;
     //validacion si imagen existe
-    if(req.files?.image){
-      const result = await uploadImage(req.files.image.tempFilePath)
-      await fs.remove(req.files.image.tempFilePath)
+    if (req.files?.image) {
+      const result = await uploadImage(req.files.image.tempFilePath);
+      await fs.remove(req.files.image.tempFilePath);
       image = {
         url: result.secure_url,
-        public_id: result.public_id
-      }
+        public_id: result.public_id,
+      };
     }
 
     const newPost = new Post({ title, description, image: image });
@@ -36,11 +35,9 @@ export const updatePost = async (req, res) => {
 
     //agrega
     const post = await Post.findById(req.params.id)
-    await deleteImage(post.image.public_id)
-
-
-    if(req.files?.image){
-
+    if(req.files){
+      console.log("hay archivos")
+      await deleteImage(post.image.public_id)
       const result = await uploadImage(req.files.image.tempFilePath)
       await fs.remove(req.files.image.tempFilePath)
       req.body.image = {
@@ -48,7 +45,14 @@ export const updatePost = async (req, res) => {
         public_id: result.public_id
       }
 
-    }
+    }/* else{
+      postnuevo = {
+        title: req.body.title,
+        description: req.body.description,
+        image: post.image
+      }
+      console.log(postnuevo)
+    } */
     
     const updatedPost = await Post.findByIdAndUpdate(
       id,
@@ -57,37 +61,33 @@ export const updatePost = async (req, res) => {
         new: true,
       }
       )
-      
-      console.log(Post.findById(id))
-
+      console.log("viejo",post)
+      console.log("update",updatedPost)
     return res.json(updatedPost);
   } catch (error) {}
   res.send("posts update");
 };
 
-export const deletePost = async(req, res) => {
-    try {
-        const postRemoved = await Post.findByIdAndDelete(req.params.id)
-        if(!postRemoved) return res.sendStatus(404);
-        
-        if(postRemoved.image.public_id){
-          await deleteImage(postRemoved.image.public_id)
-         }
-        
-        return res.sendStatus(204)
-    } catch (error) {
-        
+export const deletePost = async (req, res) => {
+  try {
+    const postRemoved = await Post.findByIdAndDelete(req.params.id);
+    if (!postRemoved) return res.sendStatus(404);
+
+    if (postRemoved.image.public_id) {
+      await deleteImage(postRemoved.image.public_id);
     }
+
+    return res.sendStatus(204);
+  } catch (error) {}
 };
-export const getPostById = async(req, res) => {
-try {
+export const getPostById = async (req, res) => {
+  try {
     //thows new Error("message")
-    const post = await Post.findById(req.params.id)
-    if(!post) return res.sendStatus(404)
-    return res.json(post)
-    
-} catch (error) {
-    console.log(error.message)
-    return res.status(500).json({"message": error.message})
-}
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.sendStatus(404);
+    return res.json(post);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  }
 };
